82aa46f981d528189e2410fbbfaa6d8c5e3a4b72
markus philipp <m.philipp1991@web.de>
2019-05-11 20:13:11 +0200

    implement addQuestion Form

---
+++ src/components/AddQuestion.vue
@@ -0,0 +1,97 @@
+<template>
+  <form>
+    <div class="form-input">
+      <label>Question Text</label>
+      <input type="text" v-model="newQuestion.text">
+    </div>
+    <div class="form-input">
+      <label>Question Type</label>
+      <select type="type" v-model="newQuestion.type">
+        <option disabled value>Select question type</option>
+        <option value="open">Open question</option>
+        <option value="choice">Multiple choice</option>
+        <option value="scale">Scale Question</option>
+      </select>
+    </div>
+    <div class="form-div" v-if="newQuestion.type === 'choice'">
+      <div class="form-input">
+        <input type="answers" v-model="answerInput">
+        <button @click.prevent="newQuestion.answers.push(answerInput)">add answer</button>
+      </div>
+      <div class="form-checkbox">
+        <input type="checkbox" id="explanation" v-model="newQuestion.options.explanation">
+        <label for="explanation">add textfield for explanation</label>
+        <input type="checkbox" id="customanswer" v-model="newQuestion.options.customAnswer">
+        <label for="customanswer">allow user to add answer</label>
+      </div>
+    </div>
+    <div class="form-div" v-if="newQuestion.type === 'scale'">
+      <label>Start Value</label>
+      <input type="text" v-model="newQuestion.options.startValue">
+      <label>End Value</label>
+      <input type="text" v-model="newQuestion.options.endValue">
+      <label>Number of Steps</label>
+      <input type="number" min="3" max="9" v-model="newQuestion.options.scaleSteps">
+    </div>
+    <button @click.prevent="emitQuestion">Add Question</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddQuestion",
+  data: function() {
+    return {
+      newQuestion: {
+        text: null,
+        type: null,
+        answers: [],
+        options: {
+          explanation: false,
+          customAnswer: false,
+          startValue: "Totally Agree",
+          endValue: "Toatally Disagree",
+          scaleSteps: 5
+        }
+      },
+      answerInput: null
+    };
+  },
+  methods: {
+    emitQuestion() {
+      this.$emit("addQuestion", this.newQuestion);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+input,
+select {
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+  border: 1px inset #42b983;
+}
+.form-input {
+  width: 90%;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+.form-div {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
---
+++ src/views/QuestionOverview.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="questionsOverview">
+    <ul>
+      <li v-for="question in questions" v-bind:key="question.id">{{question.text}}</li>
+    </ul>
+    <AddQuestion @addQuestion="addQuestionHandler"/>
+  </div>
+</template>
+
+<script>
+import AddQuestion from "@/components/AddQuestion.vue";
+export default {
+  name: "questionsOverview",
+  components: {
+    AddQuestion
+  },
+  data: function() {
+    return {
+      questions: []
+    };
+  },
+  methods: {
+    addQuestionHandler(value) {
+      //The way I set this up created an weird error: all objects in the questions array are somehow connected to the child-input!
+      let addedQuestion = value;
+      addedQuestion.id = Math.floor(Math.random() * 9000000);
+      console.log(addedQuestion);
+      this.questions.push(addedQuestion);
+    }
+  }
+};
+</script>
