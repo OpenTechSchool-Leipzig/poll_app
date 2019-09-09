39ae017145a66aea80ab78c82c211546d9cd734e
Dan McAtee <info@danmcatee.com>
2019-09-09 20:09:22 +0200

    Add bulma

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store/store';
 import { auth } from './store/firebase';
 import './registerServiceWorker';
+import '../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false;
 
