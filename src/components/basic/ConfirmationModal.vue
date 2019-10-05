1b1d2830f8e086850beea05be609d07a0939126f
markusphil <m.philipp1991@web.de>
2019-10-05 14:48:19 +0200

    prepare confirmation Modal

---
+++ src/components/basic/ConfirmationModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <default-modal v-if="visible" @closeModal="cancel">
+    <h2>{{ title }}</h2>
+    <p>{{ text }}</p>
+    <div class="level">
+      <div class="level-item">
+        <default-button name="cancel" @click="cancel" />
+      </div>
+      <div class="level-item">
+        <default-button :name="type" isDanger @click="confirm" />
+      </div>
+    </div>
+  </default-modal>
+</template>
+
+<script>
+import DefaultModal from './DefaultModal.vue';
+import DefaultButton from './DefaultButton.vue';
+
+export default {
+  components: {
+    DefaultModal,
+    DefaultButton,
+  },
+  props: {
+    // use props or vueX?
+    title: String,
+    text: String,
+    visible: Boolean,
+    type: String,
+    isDanger: Boolean,
+  },
+};
+</script>
