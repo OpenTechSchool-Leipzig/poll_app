cfe55197cf5790879308228be19f1904098ae847
Jennifer Bland <ratracegrad@gmail.com>
2019-10-29 16:35:03 -0400

    Changed watcher on value to be deep since value is an object. Added text to warn user if they select a date in the past. Added function to test if selected date is in the past

--- src/components/polls/PollPreview.vue
+++ src/components/polls/PollPreview.vue
@@ -12,12 +12,15 @@
         v-model="value.title"
         placeholder="Poll Title"
       />
-      <input
-        class="poll-preview__input poll-preview__input--date"
-        :class="{ 'poll-preview__input--highlight': isTemplateLoaded }"
-        type="date"
-        v-model="value.date"
-      />
+      <div class="date-wrapper">
+        <input
+          class="poll-preview__input poll-preview__input--date"
+          :class="{ 'poll-preview__input--highlight': isTemplateLoaded }"
+          type="date"
+          v-model="value.date"
+        />
+        <div v-if="showDateWarning" class="warning-text">Date can't be in past</div>
+      </div>
     </form>
 
     <slot></slot>
@@ -58,8 +61,18 @@ export default {
     questions() {
       this.populateAnswers();
     },
-    value() {
-      this.$emit('input', this.value);
+    value: {
+      deep: true,
+      handler: function(val) {
+        // validate date
+        if (val && val.date && this.isValidDate(val.date)) {
+          this.showDateWarning = false;
+        } else if (val && val.date) {
+          this.showDateWarning = true;
+          delete this.value.date;
+        }
+        this.$emit('input', this.value);
+      },
     },
   },
   methods: {
@@ -77,6 +90,24 @@ export default {
       // So the emit needs to be forwarde manually - it could be removed when the active poll is handled
       // in vuex-Store, which would also allow to load templates or poll from the pollOverview Page
       this.$emit('removeQuestion', id);
+    },
+    isValidDate(val) {
+      let selected = new Date(val)
+        .toISOString()
+        .substring(0, 10)
+        .split('-');
+      let today = new Date()
+        .toISOString()
+        .substring(0, 10)
+        .split('-');
+      if (selected[0] < today[0]) {
+        return false; // previous year
+      } else if (selected[0] === today[0] && selected[1] < today[1]) {
+        return false; // same year previous month
+      } else if (selected[0] === today[0] && selected[1] === today[1] && selected[2] < today[2]) {
+        return false; // same year and month earlier day
+      }
+      return true;
     },
   },
 };
@@ -116,5 +147,13 @@ export default {
 }
 header {
   @include section-header;
+}
+.date-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.warning-text {
+  font-size: 14px;
+  color: red;
 }
 </style>
