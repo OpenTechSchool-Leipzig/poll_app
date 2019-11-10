8f18ffdd86e1ea6909bf64a1478ab6710919c0d2
markusphil <m.pilipp1991@web.de>
2019-11-10 12:53:38 +0100

    Auto register base components globally

--- src/main.js
+++ src/main.js
@@ -18,6 +18,9 @@ import {
   faSortAmountUpAlt,
   faExternalLinkAlt,
 } from '@fortawesome/free-solid-svg-icons';
+
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
@@ -44,6 +47,31 @@ library.add(
   faSortAmountUpAlt,
   faExternalLinkAlt
 );
+
+const requireComponent = require.context('./components/basic', true, /[A-Z]\w+\.(vue)$/);
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName);
+  // Get PascalCase name of component
+  const componentName = upperFirst(
+    camelCase(
+      // Gets the file name regardless of folder depth
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  );
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  );
+});
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
